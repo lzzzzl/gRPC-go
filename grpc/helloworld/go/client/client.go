72dd03f7416fc91424b1dd8e3b2e5609a7907b83
zonglin.zheng <zonglin.zheng@xtalpi.com>
2023-02-05 17:29:53 +0800

    go-grpc

---
+++ grpc/helloworld/go/client/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/lzzzzl/grpc-go/grpc/helloworld/go/pb"
+	"google.golang.org/grpc"
+)
+
+const (
+	address     = "localhost:50051"
+	defaultName = "world"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewGreeterClient(conn)
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: defaultName})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
+}
---
+++ grpc/helloworld/go/service/service.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/lzzzzl/grpc-go/grpc/helloworld/go/pb"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type Server struct{}
+
+func (s *Server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &Server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
